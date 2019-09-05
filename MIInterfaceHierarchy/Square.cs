a23aed2dfeceede1aa8b94f589be2ad0fa68d098
Jacob Archambault <jacob.archambault@waystar.com>
2019-09-05 11:30:54 -0400

    Create and implement multiple interfaces.

---
+++ MIInterfaceHierarchy/Square.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIInterfaceHierarchy
+{
+    class Square : IShape
+    {
+        // Using explicit implementation to handle member name clash.
+        void IPrintable.Draw()
+        {
+            Console.WriteLine("Drawing to printer...");
+        }
+        void IDrawable.Draw()
+        {
+            Console.WriteLine("Drawing to screen...");
+        }
+        public void Print()
+        {
+            Console.WriteLine("Printing...");
+        }
+
+        public int GetNumberOfSides()
+        { return 4; }
+    }
+}
